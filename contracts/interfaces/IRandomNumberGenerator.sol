dd74cb7d9667ed6468d88733dfbfbb94e366dd59
TK-Development <timkern.lancer@gmail.com>
2021-11-04 10:15:16 +0900

    Create interfaces

---
+++ contracts/interfaces/IRandomNumberGenerator.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.4;
+
+interface IRandomNumberGenerator {
+    /**
+     * Requests randomness from a user-provided seed
+     */
+    function getRandomNumber(uint256 _seed) external;
+}
